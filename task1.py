5e2698047bfd5c8a52960784479ecedd843809db
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-05-02 21:42:22 +0400

    commit

---
+++ task1.py
@@ -0,0 +1,33 @@
+# TASK_1
+# a = int((input("rəqəm gitin..: ")))
+# b = int((input("rəqəm gitin..: ")))
+# c = int((input("rəqəm gitin..: ")))
+# d = int((input("rəqəm gitin..: ")))
+# e = int((input("rəqəm gitin..: ")))
+# data = [a, b, c, d, e]
+# data.sort()
+# print("sirali reqemler:", data)
+
+# TASK2
+# txt = input("cumle girin: ")
+# sira = sorted(txt)
+# print("metinin herflerinin sirali sekilde: " , sira)
+
+# TASK3
+# num = int(input("gizli reqemini tap: "))
+# i = 0
+# while num != 13:
+#     num = int(input("yeniden cehd edin "))
+#     i += 1
+# print("13 reqemini", i, ". cehdde tapdiniz")
+
+
+# TASK4
+# for x in range(2, 101):
+#     sade = True
+#     for i in range(2, x // 2 + 1):
+#         if x % i == 0:
+#             sade = False
+#             break
+#     if sade:
+#         print(x)
